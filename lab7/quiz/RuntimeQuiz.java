62163f111f644d34f957a114b1e299fff96427d1
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2017-10-05 18:05:38 -0700

    Submit lab7-1

--- lab7/quiz/RuntimeQuiz.java
+++ lab7/quiz/RuntimeQuiz.java
@@ -30,7 +30,7 @@ public class RuntimeQuiz {
         }
     }
 
-    public static Asymptotic f2_notation = Asymptotic.BIG_O;
+    public static Asymptotic f2_notation = Asymptotic.BIG_THETA;
     public static Runtime f2_runtime = Runtime.QUADRATIC;
     public int f2(int n) {
         if (n <= 1) return n;
@@ -38,7 +38,7 @@ public class RuntimeQuiz {
         return n + n * f2(n - 1) + n * n * f2(1);
     }
 
-    public static Asymptotic f3_notation = Asymptotic.BIG_O;
+    public static Asymptotic f3_notation = Asymptotic.BIG_THETA;
     public static Runtime f3_runtime = Runtime.LINEAR;
     /* When f3 is first called, start will be 0 and end will be the length of the array - 1 */
     public int f3(char[] array, int start, int end) {
@@ -47,7 +47,7 @@ public class RuntimeQuiz {
         return f3(array, start, mid) + f3(array, mid + 1, end);
     }
 
-    public static Asymptotic f4_notation = Asymptotic.BIG_O;
+    public static Asymptotic f4_notation = Asymptotic.BIG_THETA;
     public static Runtime f4_runtime = Runtime.LINEARITHMIC;
     /* When f4 is first called, start will be 0 and end will be the length of the array - 1 */
     public int f4(char[] array, int start, int end) {
@@ -84,7 +84,7 @@ public class RuntimeQuiz {
     public static Asymptotic f7_notation = Asymptotic.BIG_O;
     public static Runtime f7_runtime = Runtime.EXPONENTIAL;
     public static Asymptotic f7_omega_notation = Asymptotic.BIG_OMEGA;
-    public static Runtime f7_omega_runtime = Runtime.CONSTANT;
+    public static Runtime f7_omega_runtime = Runtime.LINEAR;
     /* When f7 is first called, start will be 0 and end will be the length of the array - 1 */
     public int f7(int[] array, int start, int end) {
         if (array.length <= 1 || end <= start) {
