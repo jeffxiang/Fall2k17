9e7a1a724fb9266704975686dc9083809af09798
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2017-11-17 21:26:15 -0800

    submit lab13

--- lab13/MazeBreadthFirstPaths.java
+++ lab13/MazeBreadthFirstPaths.java
@@ -1,6 +1,7 @@
-import java.util.Observable;
+import java.util.*;
+
 /**
- *  @author Josh Hug
+ *  @author Jeff Xiang
  */
 
 public class MazeBreadthFirstPaths extends MazeExplorer {
@@ -10,20 +11,57 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v, PriorityQueue<Integer> q, ArrayList<Integer> visited) {
+        marked[v] = true;
+        announce();
+        visited.add(v);
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+         for (int w : maze.adj(v)) {
+             if (!marked[w]) {
+                 q.add(w);
+                 marked[w] = true;
+                 distTo[w] = distTo[v] + 1;
+                 edgeTo[w] = v;
+                 announce();
+             }
+             if (!visited.contains(w)) {
+                 bfs(q.poll(), q, visited);
+                 if (targetFound) {
+                     return;
+                 }
+             }
+         }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        ArrayList<Integer> visited = new ArrayList<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        bfs(s, q, visited);
     }
 }
 
--- lab13/MazeCycles.java
+++ lab13/MazeCycles.java
@@ -1,24 +1,53 @@
+import java.util.ArrayList;
 import java.util.Observable;
 /**
- *  @author Josh Hug
+ *  @author Jeff Xiang
  */
 
 public class MazeCycles extends MazeExplorer {
-    /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
-    */
+
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = 0;
+        t = maze.xyTo1D(maze.N(), maze.N());
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
-    // Helper methods go here
+    public void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+                if (targetFound) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
